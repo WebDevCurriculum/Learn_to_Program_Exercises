1cf6fb4ca64334d1b653017087e55ccd5a47653f
Charity <Youngblood.charity@gmail.com>
2017-07-13 14:24:58 -0700

    added exercise for each method to arrays.rb

--- Learn_to_Program_Exercises/arrays.rb
+++ Learn_to_Program_Exercises/arrays.rb
@@ -3,3 +3,8 @@ other_peeps[3] = "Charlotte"
 other_peeps[2] = "Josephine"
 other_peeps[0] = "Christian"
 puts other_peeps 
+
+languages = ["English", "Spanish", "Ruby"]
+
+languages.each do |lang|
+	puts "I love " + lang + " !"
