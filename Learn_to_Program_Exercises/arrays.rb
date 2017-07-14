57722a6d39587b224e16c0d80d4427226c620ba0
Charity <Youngblood.charity@gmail.com>
2017-07-13 18:04:19 -0700

    added instructions to main exercise in comments

--- Learn_to_Program_Exercises/arrays.rb
+++ Learn_to_Program_Exercises/arrays.rb
@@ -33,3 +33,48 @@ puts favorites.last
 puts favorites.length 
 puts favorites.pop 
 puts favorites 
+
+=begin 
+
+Instructions for Main Exercise
+
+Building and sorting an array. 
+Write the program we talked about at the beginning of this chapter, 
+one that asks us to type as many words as we want (one word per line, 
+continuing until we just press Enter on an empty line) and then repeats the words 
+back to us in alphabetical order. 
+Make sure to test your program thoroughly; 
+for example, does hitting Enter on an empty line always exit your program? 
+Even on the first line? And the second? 
+Hint: There’s a lovely array method that will give you a sorted version of an array: sort. Use it!
+
+	1. ask for and store user input (gets.chomp) - in prompt ask user to press enter
+ 	2. user will press enter to "end" input 
+ 	3. add an if statement for pressing enter - to ensure the program ends once enter is pressed  
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
