333338d8a1400d3dbb8d434417b42fb1e0beb3ba
Charity <Youngblood.charity@gmail.com>
2017-07-13 17:59:08 -0700

    added exercise example for push, pop, last and length method

--- Learn_to_Program_Exercises/arrays.rb
+++ Learn_to_Program_Exercises/arrays.rb
@@ -22,3 +22,14 @@ puts foods.join(' :) ') + ' 8)'
 200.times do
 	puts []
 end 
+
+favorites = []
+
+favorites.push 'raindrops on roses'
+favorites.push 'whiskers on kittens'
+
+puts favorites[0]
+puts favorites.last 
+puts favorites.length 
+puts favorites.pop 
+puts favorites 
